e011ec2a49c5e3c8527fa71d6ee02730c9d0c6f5
Pedrito052002 <100108319+Pedrito052002@users.noreply.github.com>
2024-09-08 18:34:08 -0600

    Prueba

---
+++ prograweb2/api-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express') //importando librerias
+const http = require('http')
+const cors = require('cors')
+ 
+const app = express(); //tiene todas las configuraciones del servidor
+ 
+app.set('puerto' , 4200)
+app.use(express.json())
+ 
+app.get('/', (req, res)=>{
+    console.log("Revivan a Gojo")
+    res.json({'Respuesta ' : ' Buenas noshes '})
+})
+ 
+ 
+ 
+const httpServer = http.createServer(app)  //crea servidor y espera la peticion
+httpServer.listen(app.get('puerto'), "localhost", () => {
+    console.log("Server conectado")
+})
