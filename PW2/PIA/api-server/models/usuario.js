764acc74a294437307d4bdd3753324c44f97cbba
Pedrito052002 <cinthia75750@gmail.com>
2024-12-05 15:08:20 -0600

    cambio en schema

--- PW2/PIA/api-server/models/usuario.js
+++ PW2/PIA/api-server/models/usuario.js
@@ -4,7 +4,7 @@ const usuarioSchema = new mongoose.Schema({
   nombreUsuario: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   contraseña: { type: String, required: true },
-  rol: { type: String, enum: ['vendedor', 'usuario'], required: true },
+  rol: { type: String, enum: ['vendedor', 'cliente'], required: true },
   nombreCompleto: { type: String, required: true },
   telefono: { type: String },
   ciudad: { type: String },
